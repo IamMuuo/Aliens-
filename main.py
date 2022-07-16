3124bf680e765b23db184cb81e069ef104f85cf9
erick <hearteric57@gmail.com>
2022-07-16 17:51:57 +0300

    Drew a player ship onto the screen

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ Description: Contains the entry point to the game
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # Initialize game and create a screen object.
@@ -19,6 +20,9 @@ def run_game():
     game_settings = Settings()
     screen = pygame.display.set_mode((game_settings.screen_width,game_settings.screen_height))
     pygame.display.set_caption("Aliens!")
+
+    # make a ship
+    ship = Ship(screen)
 
     # start the main loop of the game
 
@@ -32,6 +36,8 @@ def run_game():
 
         # Make the most recently drawn sceen available
         screen.fill(game_settings.bg_color)
+        ship.blitme()
+
         pygame.display.flip()
 
 if __name__ == '__main__':
---
+++ ship.py
@@ -0,0 +1,23 @@
+# Contains class definition for ship
+import pygame
+
+class Ship():
+
+    def __init__(self, screen):
+        ''' Initialize the ship and set its starting position'''
+
+        self.screen = screen
+
+        # load the texture
+        self.image = pygame.image.load('images/eagle.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+
+        # start each new ship at the bottom center of the screen
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        '''Draw ship at its current location'''
+        self.screen.blit(self.image, self.rect)
